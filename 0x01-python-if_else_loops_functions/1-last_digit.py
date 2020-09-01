4d63fe6b95ec7f8679f6e48ed8648c6f9dd0b654
MIlenaMontoya <1852@holbertonschool.com>
2020-09-01 17:05:30 +0000

    pep8

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -11,7 +11,6 @@ if LastD > 5:
         print("{} {} is {} and greater than 5".format(string1, number, LastD))
 elif LastD == 0:
         print("{} {} is {} and is 0".format(string1, number, LastD))
-
 else:
         string2 = "and is less than 6 and not 0"
         print("{} {} is {} {}".format(string1, number, LastD, string2))
