c1ca310879b4c10e4dff60e089cba9dc6ea556d4
MIlenaMontoya <1852@holbertonschool.com>
2020-09-01 17:19:21 +0000

    if ... else 🤞

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,9 +8,9 @@ else:
 string1 = "Last digit of"
 
 if LastD > 5:
-        print("{} {} is {} and greater than 5".format(string1, number, LastD))
+        print("{:s} {:d} is {:d} and greater than 5".format(string1, number, LastD))
 elif LastD == 0:
-        print("{} {} is {} and is 0".format(string1, number, LastD))
+        print("{:s} {:d} is {:d} and is 0".format(string1, number, LastD))
 else:
         string2 = "and is less than 6 and not 0"
-        print("{} {} is {} {}".format(string1, number, LastD, string2))
+        print("{:S} {:d} is {:d} {:s}".format(string1, number, LastD, string2))
