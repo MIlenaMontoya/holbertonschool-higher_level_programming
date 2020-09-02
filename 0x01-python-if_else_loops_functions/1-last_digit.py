2495f55c1f21cce01b2fcdbb05aa94150be8c3cb
MIlenaMontoya <1852@holbertonschool.com>
2020-09-02 02:32:11 +0000

    if else

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -7,9 +7,9 @@ else:
         ld = number % -10
 st1 = "Last digit of "
 if ld > 5:
-        print("{:s} {:d} is {:d} and greater than 5".format(st1, number, ld))
+        print("{}{} is {} and greater than 5".format(st1, number, ld))
 elif ld == 0:
-        print("{:s} {:d} is {:d} and is 0".format(st1, number, ld))
+        print("{}{} is {} and is 0".format(st1, number, ld))
 else:
         st2 = "and is less than 6 and not 0"
-        print("{:s}{:d} is {:d} {:s}".format(st1, number, ld, st2))
+        print("{}{} is {} {}".format(st1, number, ld, st2))
---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def uppercase(str):
+        for word in str:
+                if ord(word) >= 97 and ord(word) <= 122:
+                        word = chr(ord(word) - 32)
+                print("{}".format(word), end="")
+        print("")
