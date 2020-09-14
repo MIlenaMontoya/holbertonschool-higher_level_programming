45b6e1690967ace60955d1cbc7ae91cb1b3604ef
MIlenaMontoya <1852@holbertonschool.com>
2020-09-14 02:15:52 +0000

    sad

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,2 +1,11 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if not isinstance(roman_string, str):
+        return 0
+    res = 0
+    num = 0
+    rom = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    for n in reversed(roman_string):
+        num = rom[n]
+        res += num if res < num * 5 else -num
+    return res
