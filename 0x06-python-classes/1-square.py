4c25ff6a703e973ad525a665de9f550fdffbcdfb
MIlenaMontoya <1852@holbertonschool.com>
2020-09-22 21:32:53 +0000

    square_size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+'''Declaracion class Square'''
+
+
+class Square():
+    '''class square vacia'''
+
+    def __init__(self, size):
+        '''constructor de objeto
+        con arg. '''
+
+        self.__size = size
