0e4b3091179bd81dba1860913205cd67caa390a8
MIlenaMontoya <1852@holbertonschool.com>
2020-09-23 01:58:11 +0000

    square ###

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+'''Declaracion class Square'''
+
+
+class Square():
+    '''class square vacia'''
+
+    def __init__(self, size=0):
+        '''constructor de objeto
+        con arg. '''
+
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    '''metodo get define contructor que
+    optiene el valor size'''
+
+    @property
+    def size(self):
+        return self.__size
+
+    '''metodo set que pone el valor y lo valida
+    segun el tipo de dato'''
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    '''crear nuevo constructor que
+     retorna el area de un cuadrado'''
+
+    def area(self):
+        return self.__size ** 2
+
+    '''definicion de instancia para imprimir'''
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for a in range(self.__size):
+                for b in range(self.__size):
+                    print("#", end='')
+                print()
