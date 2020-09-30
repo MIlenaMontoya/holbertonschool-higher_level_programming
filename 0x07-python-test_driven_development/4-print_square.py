a34506ef3851ad0b8360e120de684068a4c393ef
MIlenaMontoya <1852@holbertonschool.com>
2020-09-30 16:58:52 +0000

    task 3 square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+function that prints a square with the character #
+"""
+
+
+def print_square(size):
+    """Docstring
+    """
+    if type(size) != int:
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+    if type(size) is float and size < 0:
+        raise TypeError("size must be an integer")
+    for a in range(size):
+        for b in range(size):
+            print('#', end="")
+        print()
