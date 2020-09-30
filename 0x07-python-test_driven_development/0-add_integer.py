8ac5370bb42bc489cb12921ff784bbf09eddcbc6
MIlenaMontoya <1852@holbertonschool.com>
2020-09-30 18:38:40 +0000

    more line

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
-"""Write a function that adds 2 integers
+"""
+Write a function that adds 2 integers
+
+
 """
 
 
 def add_integer(a, b=98):
     """function that adds 2 integers or floats
-    whit paramethers type int
+    whit paramethers type int.
     """
 
     if type(a) != int and type(a) != float:
