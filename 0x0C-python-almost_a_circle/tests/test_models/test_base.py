e367c23b9efca563f06407690d9d1f383eb0f512
MIlenaMontoya <1852@holbertonschool.com>
2020-10-14 02:57:48 +0000

    test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,28 +1,49 @@
 #!/usr/bin/python3
-"""This module holds the class for unit test
-    teting the class Base
-    """
+""" Unittest for base.py """
 import unittest
-import os
-import pep8
 from models.base import Base
-from models.rectangle import Rectangle
-from models.square import Square
-
-
-class Test_base(unittest.TestCase):
-    """Class with unitests for class Base
-    Args:
-        unittest ([class]): Class for unittest
-    """
-
-    def test_base_pep8(self):
-        """Check PEP8 style
-        """
-        pep8style = pep8.StyleGuide(quiet=True)
-        result = pep8style.check_files(['models/base.py'])
-        self.assertEqual(result.total_errors, 0)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
+
+
+class TestBase(unittest.TestCase):
+    """Testcase is created by subclassing unittest.TestCase"""
+    def setUp(self):
+        """Prepare the test fixture - Assign private class attribute"""
+        Base._Base__nb_objects = 0
+
+    def test_create_instance_id_int(self):
+        """Testing id property with int type"""
+        tbase = Base(10)
+        self.assertEqual(tbase.id, 10)
+
+    def test_create_instance_id_int_neg(self):
+        """Testing id property with negative int type"""
+        tbase = Base(-10)
+        self.assertEqual(tbase.id, -10)
+
+    def test_create_instance_id_float(self):
+        """Testing id property with float type"""
+        tbase = Base(2.15)
+        self.assertEqual(tbase.id, 2.15)
+
+    def test_create_instance_id_empty(self):
+        """Testing id property withouth attribute"""
+        tbase = Base()
+        self.assertEqual(tbase.id, 1)
+
+    def test_create_instance_id_None(self):
+        """Testing id property with None type"""
+        tbase = Base(None)
+        self.assertEqual(tbase.id, 1)
+
+    def test_create_instance_id_moreargs(self):
+        """Testing id property with more than one argument"""
+        with self.assertRaises(TypeError):
+            tbase = Base(6, 3)
+
+    def test_check_private_attribute(self):
+        """Checking private attribute __nb_objects"""
+        tbase = Base()
+        with self.assertRaises(AttributeError):
+            tbase.__nb_objects
