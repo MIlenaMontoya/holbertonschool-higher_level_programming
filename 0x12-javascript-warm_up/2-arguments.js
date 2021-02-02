c1f219de5df22fca3d90e21ee9866b0b62dde04b
MIlenaMontoya <1852@holbertonschool.com>
2021-02-02 12:32:18 -0500

    javascript

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+let myArgs = process.argv;
+if (myArgs.length === 2) {
+    console.log('No argument');
+}
+el
+se
+if (myArgs.length === 3) {
+    console.log('Argument found');
+} else {
+    console.log('Arguments found');
+}
