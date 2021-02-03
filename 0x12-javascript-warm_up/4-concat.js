fe4c9d6277328cd57eb62ce83437e9d4a755cdc6
MIlenaMontoya <1852@holbertonschool.com>
2021-02-02 20:47:01 -0500

    files

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+// Prints the two arguments passed to it
+
+const args = process.argv;
+console.log(args[2] + ' is ' + args[3]);
---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// Uses an array to loop through and print strings
+
+const myArray = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < myArray.length; i++) {
+  console.log(myArray[i]);
+}
