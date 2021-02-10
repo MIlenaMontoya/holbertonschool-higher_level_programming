9b6942b509bd32cd8f3570d23277404631096e7d
MIlenaMontoya <1852@holbertonschool.com>
2021-02-09 23:40:48 -0500

    fix

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
+// Prints the title of a Star Wars movie where the episode number
+// matches the given integer
 
 const request = require('request');
-const urlApi = 'https://swapi.co/api/films/' + process.argv[2];
 
-request(urlApi, function (error, response, body) {
-  if (error) {
-    console.log(error); // Print the error if one occurred
-  } else {
-    console.log(JSON.parse(body).title);
-  }
+const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
+request.get(url, function (err, reply, body) {
+  if (err) throw err;
+  const content = JSON.parse(body);
+  console.log(content.title);
 });
